801e03241ecc66be8bd8f6b957d7846c96dfd81b
3202448109@qq.com <3202448109@qq.com>
2021-03-26 00:52:57 +0800

    Initial commit

---
+++ src/components/Second.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h3>用户管理</h3>
+
+        <table border="1" cellpadding="0">
+            <tr>
+                <td>ID</td>
+                <td>姓名</td>
+                <td>生日</td>
+                <td>操作</td>
+            </tr>
+            <tr v-for="(user, index) in user_list" :key="index">
+                <td>{{user.id}}</td>
+                <td>{{user.username}}</td>
+                <td>{{user.birthday}}</td>
+                <td><a href="javascript:void(0)" @click="del(index)">删除</a> |
+                    <!--         传递的id是属性 则需要v-bind绑定           -->
+                    <router-link :to="`/detail/${JSON.stringify(user_list[index])}`">查看用户详情</router-link>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Second",
+        data() {
+            return {
+                user_list: [
+                    {id: 1, username: "小黑", birthday: "2022-12-12", content: "自我介绍"},
+                    {id: 2, username: "小包", birthday: "2032-12-12", content: "自我介绍"},
+                    {id: 3, username: "小子", birthday: "2042-12-12", content: "自我介绍"},
+                    {id: 4, username: "小率", birthday: "2052-12-12", content: "自我介绍"},
+                ]
+            }
+        },
+        methods: {
+            del(index) {
+                this.user_list.splice(index,1);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    table {
+        text-align: center;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3>用户详情页</h3>
+<!--        需求：在用户详情页展示该用户的详细信息-&ndash;&gt; {{ JSON.parse($route.params.data)  }}-->
+        <!--    遍历查询用户的详细信息  展示出来    -->
+        <span v-for="(value, key) in JSON.parse($route.params.data)" :key="key">
+            {{ key }}: {{ value }}<br>
+        </span>
+        <button @click="back_index">返回首页</button>
+        <button @click="back_user_info">返回用户列表页</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserDetail",
+    data() {
+        return {}
+    },
+    methods: {
+        back_index() {
+            this.$router.push({path: '/'});
+        },
+        back_user_info() {
+          this.$router.push({path: '/second'});
+        },
+    }
+}
+</script>
+
+<style scoped>
+</style>
